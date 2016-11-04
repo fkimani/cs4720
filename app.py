84e431619e8add16146cc55c38e9c79df578c962
F Kimani <kimanifm@yahoo.com>
2016-11-04 05:42:30 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ app = Flask(__name__)
 
 @app.route('/temperature', methods=['GET', 'POST'])
 def temperature():
-	#zipcode = request.from['zip']
-	zip = requests.get("http://localhost:5000/")
-	user_apiid = '' #use your APIID
-	r = requests.get("http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&APPID='+user_apiid +'")
+	zipcode = request.form['zip']
+	#zipc = requests.get("http://localhost:5000/")
+	user_apiid = '5cb00286a7cf3a8f11164ed76bcaf93e'
+	#r = requests.get("http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&APPID='+user_apiid +'")
+	r = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=30011,us&units=imperial&APPID="+user_apiid)
 	json_object = r.json()
-	temp_k = float(json_object['main']['temp'])
-	temp_f = (temp_k - 273.15) * 1.8 +3.2
-	return render_template('temperature.html', temp=temp_f)
+	#temp_k = float(json_object['main']['temp'])
+	#temp_f = (temp_k - 273.15) * 1.8 +3.2
+	#fake_temp_f = 314 #is this a str or int?
+	#temp_f = fake_temp_f
+	temp_f = float(json_object['main']['temp'])
+	tester = 'tester'
+	return render_template('temperature.html',  temp=temp_f, zipc = zipcode)
 
 @app.route('/')
 def index():
