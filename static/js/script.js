c0761f602609dcf196277f84df53e4b2016ac30a
F Kimani <kimanifm@yahoo.com>
2016-12-07 00:57:47 -0500

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,37 @@
 	//this function will validate user input 
-	//revisit to prevent user from being sent to results page if assertion fails. thinking about assert, look into that
 	function runner()
-	{
-		var x = document.getElementById("zip_input").value;
-		var i = 0;
-		for(i in x){
-			if ( isNaN(parseInt(x[i])) )
-			{
-				alert ("please enter correct zip code");
-				break;							
+		{
+			var x = document.getElementById("zip_input").value;
+			var i = 0;
+			for(i in x){
+				if ( isNaN(parseInt(x[i])) )
+				{
+					alert ("Please enter a valid US zip code");
+					document.getElementById("zip_input").value =" ";
+					break;
+				}
+				else if(x.length < 5)
+				{
+					alert ("Please enter a 5 digit US zip code");
+					document.getElementById("zip_input").value =" ";
+					break;
+				}
+				else if(x.length == 0)
+				{
+					alert ("Please enter a 5 digit US zip code");
+					document.getElementById("zip_input").value =" ";
+					break;
+				}
+				else if(x == " ")
+				{
+					alert ("Blank input! Please enter a 5 digit US zip code");
+					document.getElementById("zip_input").value ="";
+					break;
+				}
 			}
-		}	
-	}
+		
+		}
 	//event handler for runner function
-	document.getElementById("submit_btn").addEventListener('click', runner, false)
+	document.getElementById("submit_btn").addEventListener('click', runner(), false)
 
 
